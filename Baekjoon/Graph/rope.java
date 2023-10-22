4cd161c32e125c16496a8bdf7b6b74dc722c2cf6
sds <sds8154@tukorea.ac.kr>
2023-10-22 17:48:44 +0900

    #1789

--- Baekjoon/Graph/rope.java
+++ Baekjoon/Graph/rope.java
@@ -2,9 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 
-public class Main {
+public class rope {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
