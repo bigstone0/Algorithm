3bf13cde88adaa1db18cbad92cf8fceca468ef8e
sds <sds8154@tukorea.ac.kr>
2023-12-26 23:40:17 +0900

    #9456

--- Baekjoon/dp/LCS.java
+++ Baekjoon/dp/LCS.java
@@ -7,29 +7,29 @@ public class LCS {
     static int[][] dp;
     static int max;
     static int result;
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        first=bf.readLine().split("");
-        second=bf.readLine().split("");
-        dp=new int[first.length+1][second.length+1];
 
-        for(int i=0;i<first.length;i++){
-            for(int j=0;j<second.length;j++){
-                check(i,j);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        first = bf.readLine().split("");
+        second = bf.readLine().split("");
+        dp = new int[first.length + 1][second.length + 1];
+
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                check(i, j);
             }
         }
 
         System.out.println(max);
     }
 
-    static void check(int a, int b){
-        if(first[a].equals(second[b])){
-            dp[a+1][b+1]=dp[a][b]+1;
-        }
-        else{
-            dp[a+1][b+1]=Math.max(dp[a][b+1],dp[a+1][b]);
+    static void check(int a, int b) {
+        if (first[a].equals(second[b])) {
+            dp[a + 1][b + 1] = dp[a][b] + 1;
+        } else {
+            dp[a + 1][b + 1] = Math.max(dp[a][b + 1], dp[a + 1][b]);
         }
 
-        max=Math.max(max,dp[a+1][b+1]);
+        max = Math.max(max, dp[a + 1][b + 1]);
     }
 }
