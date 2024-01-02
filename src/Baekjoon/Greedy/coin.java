1ccbe948dfb0c1736d2de953daa4b880e16699f1
sds <sds8154@tukorea.ac.kr>
2024-01-02 17:07:43 +0900

    .

---
+++ src/Baekjoon/Greedy/coin.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+// #11047(silver4)
+public class coin {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = 0;
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++)
+            A[i] = scan.nextInt();
+
+        while (K != 0) {
+            for (int i = A.length - 1; i >= 0; i--) {
+                if (K >= A[i]) {
+                    count = count + (K / A[i]);
+                    K = K % A[i];
+                    if (K == 0)
+                        break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/Baekjoon/dp/LongSeq.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+// #11053(silver2)
+public class LongSeq {
+    static int N;
+    static int[] seq;
+    static int[] dp;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        seq = new int[N];
+        dp=new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken(" "));
+        }
+
+        for (int i = 0; i <N; i++) {
+            if(dp[i]==0) max = Math.max(seqSort(i),max);
+        }
+        System.out.println(max+1);
+    }
+
+    static int seqSort(int start) {
+        if(dp[start]!=0) return dp[start];
+        for (int i = start; i <N; i++) {
+            if (seq[i] > seq[start]) {
+                dp[start]=Math.max(seqSort(i)+1,dp[start]);
+            }
+        }
+        return dp[start];
+    }
+}
