6e5e95671ca1f92a20e975fc8875dd022103f94b
sds <sds8154@tukorea.ac.kr>
2024-01-15 16:58:28 +0900

    .

--- Programmers/Graph/WordChange.java
+++ Programmers/Graph/WordChange.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class WordChange {
     public static void main(String[] args) {
         String[] words = {"hot", "dot", "dog", "lot", "log"};
         System.out.println(solution("hit", "cog", words));
